7c5383a4eaa85e835c5a177fa557b5fda39c5e91
DanSoup <dansoup@hotmail.co.uk>
2019-09-18 15:41:00 +0100

    added an example article to work on formatting

--- src/components/eg/eg.js
+++ src/components/eg/eg.js
@@ -5,45 +5,90 @@ import './article.css';
 
 const Eg = (props) => {
 
-  return <div className="article">
-    <h1>IF STATEMENTS - BASIC</h1>
-    <h2>JavaScript</h2>
-    <ul className="contents">
-      <li><a href="#the-structure">The Structure</a></li>
-      <li><a href="#real-world-example">Real World Example</a></li>
-      <li><a href="#examples">Examples</a></li>
-    </ul>
-    <p>"If" statements in JavaScript allow us to control which lines of code execute and which ones don't. This is a form of "flow control", controlling the flow of the code. The most basic if statement executes a block of code is certain conditions are met.</p>
-    <a name="real-world-example"/><h3>Real World Example</h3>
-    <p>I'm at home enjoying a day off. And I ask myself if I am hungry. If I am hungry, I will eat a sandwich. The pseudocode for this might look as follow.</p>
-    <code>IF I am hungry THEN eat sandwich.</code>
-    <p>I only eat the sandwich if I am hungry. I don't even consider eating the sandwich if I'm not hungry.</p>
-    <figure>
-      <img src={image} alt="Trulli"/>
-      <figcaption>I am, of course, always hungry.</figcaption>
-    </figure> 
-    <a name="the-structure"/><h3>The Structure</h3>
-    <p>The basic if statement looks like this:</p>
-    <code>{[
-      'if (condition) {',
-      '  code to execute',
+  const ColoredCode = () => {
+
+    // if (me === 45) {
+    //   new Class
+    //   'grg'
+    //   /gregr/
+    //   // fegrgtd
+    // let aFunc = () => {};
+    // }
+
+    const code = [
+      '~pif~ (~cme~ === ~l45~) {',
+      '  ~bnew~ ~tClass~',
+      '  ~o\'grg\'~',
+      '  ~r/gregr/~',
+      '  ~g// fegrgtd~',
+      '  ~blet~ ~yaFunc~ = () ~b=>~ {};',
       '}'
-    ].join('\n')}
-    </code>
-    <p>The "condition" will be evaluated and if, it's evaluated to being truthy, the following code block is executed. As with any code block this could be one or more lines of code. If the condition is evaluated to falsy the code block is not executed.</p>
-    <a name="examples"/><h3>Examples</h3>
-    <code>{[
-      'const hungry = true;',
-      'if (hungry) {',
-      '  console.log(\'I will eat a sandwich\')',
-      '}',
-      'console.log(\'I will carry on with my day\')'
-    ].join('\n')}</code>
-    <p>This is the code for our sandwich example above. The variable "hungry" is initiated with the boolean value "true". The condition evaluates as truthy because "hungry" is true and the log will output "I will eat a sandwich" followed by "I will carry on with my day".</p>
-    <p>Published: 10/09/2019</p>
-    <p>First Published: 09/09/2019</p>
-    <p>Author: Dan Soup</p>
-    <p>Contributors: Romy</p>
+    ];
+
+
+
+    return <code>
+      {code.map(line => {
+
+        const newLine = line.replace(/~([a-z])(.+?)~/g, '<span class="code-$1">$2</span>')
+
+        return <p dangerouslySetInnerHTML={{__html: newLine}}></p>;
+
+      })}
+    </code> 
+  }
+
+  return <div className="article">
+    <div className="inner-article">
+
+      <h1>IF STATEMENTS - BASIC</h1>
+      <h2>JavaScript</h2>
+      <ul className="contents">
+        <li><a href="#the-structure">The Structure</a></li>
+        <li><a href="#real-world-example">Real World Example</a></li>
+        <li><a href="#examples">Examples</a></li>
+      </ul>
+      <p>"If" statements in JavaScript allow us to control which lines of code execute and which ones don't. This is a form of "flow control", controlling the flow of the code. The most basic if statement executes a block of code is certain conditions are met.</p>
+      
+      {/* Real World Examples */}
+      <a name="real-world-example"/><h3>Real World Example</h3>
+      <p>I'm at home enjoying a day off and I ask myself if I am hungry. If I am hungry, I will eat a sandwich. The pseudocode for this might look as follow.</p>
+      <code>IF I am hungry THEN eat sandwich.</code>
+      <p>I only eat the sandwich if I am hungry. I don't even consider eating the sandwich if I'm not hungry.</p>
+      <figure>
+        <img src={image} alt="Trulli"/>
+        <figcaption>I am, of course, always hungry.</figcaption>
+      </figure>
+      
+      {/* The Structure */}
+      <a name="the-structure"/><h3>The Structure</h3>
+      <p>The basic if statement looks like this:</p>
+      <ColoredCode/>
+      <code>{[
+        'if (condition) {',
+        '  code to execute',
+        '}'
+      ].join('\n')}
+      </code>
+      <p>The "condition" will be evaluated and if, it's evaluated to being truthy, the following code block is executed. As with any code block this could be one or more lines of code. If the condition is evaluated to falsy the code block is not executed.</p>
+      
+      {/* Examples */}
+      <a name="examples"/><h3>Examples</h3>
+      <code>{[
+        'const hungry = true;',
+        'if (hungry) {',
+        '  console.log(\'I will eat a sandwich\')',
+        '}',
+        'console.log(\'I will carry on with my day\')'
+      ].join('\n')}</code>
+      <p>This is the code for our sandwich example above. The variable "hungry" is initiated with the boolean value "true". The condition evaluates as truthy because "hungry" is true and the log will output "I will eat a sandwich" followed by "I will carry on with my day".</p>
+      
+      {/* Metadata */}
+      <p>Published: 10/09/2019</p>
+      <p>First Published: 09/09/2019</p>
+      <p>Author: Dan Soup</p>
+      <p>Contributors: Romy</p>
+    </div>
   </div>
 
 }
