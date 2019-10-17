88cb7065710524123c792fb996d1c0d5ed541c0c
DanSoup <dansoup@hotmail.co.uk>
2019-10-17 18:43:14 +0100

    simplified react fragment

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 const Home = (props) => {
 
-  return <React.Fragment>
+  return <>
     <h1>Welcome to PSOUPOCODE</h1>
     <h2>What is PSOUPOCODE?</h2>
     <p>While mentoring people in JavaScript I realized a lot of coding tutorials could be better. They assume prior knowledge, gloss over important concepts while focusing on rarely used ones (I have used a switch statement maybe once in the last year) and while they give a good foundation for working with coding they have some holes. PSOUPOCODE is my attempt to write tutorials that make sense and teach concepts in the correct order.</p>
-  </React.Fragment>
+  </>
 
 }
 
